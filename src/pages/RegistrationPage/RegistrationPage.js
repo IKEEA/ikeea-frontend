ca1475c7d7e197a53340699a940828125c3be568
KasparasKralikas <kasparaskr@unity3d.com>
2020-03-22 14:53:17 +0200

    Installed Material UI package

--- src/pages/RegistrationPage/RegistrationPage.js
+++ src/pages/RegistrationPage/RegistrationPage.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './RegistrationPage.scss';
 
 function RegistrationPage() {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [password, setPassword] = useState('');
+ 
   return (
     <div>
     registration
