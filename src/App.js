40e33fda38c1d18a170b6b5230432a6f33ceb27c
Egle Puodziunaite <Egle.Puodziunaite@ibm.com>
2020-04-12 18:21:55 +0300

    add spinner

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ import LoginPage from './pages/LoginPage/LoginPage';
 import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
 import ErrorPage from './pages/ErrorPage/ErrorPage';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import { ErrorsContext } from './context/ErrorsContext';
 import { UserContext } from './context/UserContext';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import axios from 'axios';
+
+import { useStyles } from './App.styles';
 
 const theme = createMuiTheme({
   palette: {
@@ -30,6 +33,9 @@ const App = () => {
   const [errors, setErrors] = useState({});
   const [user, setUser] = useState({ roles: ['UNAUTHORIZED'] });
   const [loginError, setLoginError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const classes = useStyles();
 
   useEffect(() => {
     axios.defaults.headers.common = {'Authorization': localStorage.getItem('token')}
@@ -52,12 +58,15 @@ const App = () => {
    }
 
   function getUserProfile() {
+    setLoading(true);
     axios
     .get(`${process.env.REACT_APP_SERVER_URL}/api/user/profile`)
     .then(res => {
       setUser(res.data);
+      setLoading(false);
     })
     .catch(err => {
+      setLoading(false);
       // setLoginError('Error while getting profile information, please try again later.');
     });
   }
@@ -66,7 +75,10 @@ const App = () => {
     <UserContext.Provider value={user}>
       <ErrorsContext.Provider value={[errors, setErrors]}>
         <MuiThemeProvider theme={theme}>
-          <BrowserRouter>
+          {
+            loading ?
+            <CircularProgress size={100} thickness={5} className={classes.spinner}/> :
+            <BrowserRouter>
             <Switch>
               <Route exact path='/' component={MainPage} />
               <AuthRoute path='/login' user={user}>
@@ -78,6 +90,7 @@ const App = () => {
               <Redirect to='/' />
             </Switch>
           </BrowserRouter>
+          }
         </MuiThemeProvider>
       </ErrorsContext.Provider>
     </UserContext.Provider>
---
+++ src/App.styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles(theme => ({
+    spinner: {
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+      margin: 'auto'
+    }
+  }));
