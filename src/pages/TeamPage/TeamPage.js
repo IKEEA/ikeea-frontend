1ae9a1c1f081b39144f77944b390d515e8d438a4
KasparasKralikas <kasparaskr@unity3d.com>
2020-04-16 10:49:45 +0300

    Switched to using Dialog instead of Modal

--- src/pages/TeamPage/TeamPage.js
+++ src/pages/TeamPage/TeamPage.js
@@ -2,33 +2,58 @@ import React, { useState, useContext } from 'react';
 import { UserContext } from './../../context/UserContext';
 import Menu from '../../components/Menu/Menu';
 import Button from '@material-ui/core/Button';
-import Modal from '@material-ui/core/Modal';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import { useStyles } from './TeamPage.styles';
 
+
 const TeamPage = () => {
-  const [invitationModal, setInvitationModal] = useState(false);
+  const [invitationDialog, setInvitationDialog] = useState(false);
   const [user, setUser] = useContext(UserContext);
 
   const classes = useStyles();
 
-  const handleInviationModalOpen = () => {
-    setInvitationModal(true);
+  const handleInviationDialogOpen = () => {
+    setInvitationDialog(true);
   };
 
-  const handleInviationModalClose = () => {
-    setInvitationModal(false);
+  const handleInviationDialogClose = () => {
+    setInvitationDialog(false);
   };
 
   return (
     <div>
       <Menu>
-        <Button variant="contained" color="primary" raised="true" onClick={handleInviationModalOpen}>Invite New User</Button>
+        <Button variant="contained" color="primary" raised="true" onClick={handleInviationDialogOpen}>Invite</Button>
       </Menu>
-      <Modal
-        open={invitationModal}
-        onClose={handleInviationModalClose}>
-        <div>Test</div>
-      </Modal>
+      <Dialog open={invitationDialog} onClose={handleInviationDialogClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Send Invitation</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To send an invitation to a new employee, please enter their email address:
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleInviationDialogClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleInviationDialogClose} color="primary">
+            Invite
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
