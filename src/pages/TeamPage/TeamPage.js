33dc867b36b41d79afd84a1afa30ce8dedc2f05f
KasparasKralikas <kasparaskr@unity3d.com>
2020-04-16 16:56:40 +0300

    Added logic for sending invitation

--- src/pages/TeamPage/TeamPage.js
+++ src/pages/TeamPage/TeamPage.js
@@ -8,12 +8,18 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Snackbar from '@material-ui/core/Snackbar';
+import Alert from '@material-ui/lab/Alert';
 import { useStyles } from './TeamPage.styles';
+import * as validator from '../../helpers/inputValidator';
+import axios from 'axios';
 
 
 const TeamPage = () => {
-  const [invitationDialog, setInvitationDialog] = useState(false);
   const [user, setUser] = useContext(UserContext);
+  const [invitationDialog, setInvitationDialog] = useState(false);
+  const [email, setEmail] = useState({ input: null, error: false, helperText: null });
+  const [alert, setAlert] = useState({ open: false, message: null, severity: null });
 
   const classes = useStyles();
 
@@ -25,8 +31,32 @@ const TeamPage = () => {
     setInvitationDialog(false);
   };
 
+  const sendInvitation = () => {
+
+    const errors = [];
+    errors.push(validator.validateField(email.input, setEmail, validator.validateEmail));
+    if (!errors.find(error => error === true)) {
+      axios
+        .post(`${process.env.REACT_APP_SERVER_URL}/api/user/invite?email=${email.input.value}`)
+        .then(res => {
+          handleInviationDialogClose();
+          setAlert({ open: true, message: 'An invitation email for the employee was sent successfully!', severity: 'success' });
+        })
+        .catch(err => {
+          handleInviationDialogClose();
+          console.log(err);
+          setAlert({ open: true, message: err.message, severity: 'error' });
+        });
+    }
+  }
+
   return (
     <div>
+      <Snackbar open={alert.open} autoHideDuration={600000} onClose={() => setAlert({ open: false, message: null })} anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}>
+        <Alert onClose={() => setAlert({ open: false, message: null })} severity={alert.severity}>
+          {alert.message}
+        </Alert>
+      </Snackbar>
       <Menu>
         <Button variant="contained" color="primary" raised="true" onClick={handleInviationDialogOpen}>Invite</Button>
       </Menu>
@@ -37,19 +67,19 @@ const TeamPage = () => {
             To send an invitation to a new employee, please enter their email address:
           </DialogContentText>
           <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
+            label="Email"
             fullWidth
+            type="email"
+            inputRef={input => email.input = input}
+            error={email.error}
+            helperText={email.helperText}
           />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleInviationDialogClose} color="primary">
             Cancel
           </Button>
-          <Button onClick={handleInviationDialogClose} color="primary">
+          <Button onClick={sendInvitation} color="primary">
             Invite
           </Button>
         </DialogActions>
