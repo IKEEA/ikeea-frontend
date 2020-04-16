9f3699bc9da42a82351d926c0bcf59d77c8706fc
KasparasKralikas <kasparaskr@unity3d.com>
2020-04-16 10:37:13 +0300

    Added basis for invitation modal

--- src/pages/TeamPage/TeamPage.js
+++ src/pages/TeamPage/TeamPage.js
@@ -1,14 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { UserContext } from './../../context/UserContext';
 import Menu from '../../components/Menu/Menu';
+import Button from '@material-ui/core/Button';
+import Modal from '@material-ui/core/Modal';
+import { useStyles } from './TeamPage.styles';
 
 const TeamPage = () => {
+  const [invitationModal, setInvitationModal] = useState(false);
   const [user, setUser] = useContext(UserContext);
-  console.log(user);
+
+  const classes = useStyles();
+
+  const handleInviationModalOpen = () => {
+    setInvitationModal(true);
+  };
+
+  const handleInviationModalClose = () => {
+    setInvitationModal(false);
+  };
+
   return (
-    <Menu>
-      Team page
-    </Menu>
+    <div>
+      <Menu>
+        <Button variant="contained" color="primary" raised="true" onClick={handleInviationModalOpen}>Invite New User</Button>
+      </Menu>
+      <Modal
+        open={invitationModal}
+        onClose={handleInviationModalClose}>
+        <div>Test</div>
+      </Modal>
+    </div>
   );
 }
 
