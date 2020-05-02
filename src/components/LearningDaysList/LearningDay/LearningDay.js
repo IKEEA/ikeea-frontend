bd88a013de83b71a7af41bc8288483b942730044
KasparasKralikas <kasparaskr@unity3d.com>
2020-05-02 12:19:28 +0300

    Added confirmation dialog for deletion of learning day

--- src/components/LearningDaysList/LearningDay/LearningDay.js
+++ src/components/LearningDaysList/LearningDay/LearningDay.js
@@ -33,6 +33,7 @@ const LearningDay = ({ setLearningDayModal, learningDayEditable, learningDayNew,
     const [subtopics, setSubtopics] = useState([]);
     const [topicsOpen, setTopicsOpen] = useState(false);
     const [subtopicsOpen, setSubtopicsOpen] = useState(false);
+    const [deleteDialog, setDeleteDialog] = useState(false);
     const classes = useStyles();
 
     useEffect(() => {
@@ -82,143 +83,162 @@ const LearningDay = ({ setLearningDayModal, learningDayEditable, learningDayNew,
     }
 
     const handleDeleteLearningDay = (e) => {
+        setDeleteDialog(false);
         deleteLearningDay(learningDay.id);
     }
 
     return (
-        <Dialog fullWidth="lg" maxWidth="lg" onClose={(e) => handleLearningDayClose(e)} open={(e) => handleLearningDayClose(e)} classes={{ paper: classes.LearningDayModal }}>
-            <DialogTitle className={classes.dialogTitle}>
-                <TextField
-                    className={classes.title}
-                    required
-                    placeholder="Title"
-                    value={title}
-                    onChange={e => setTitle(e.target.value)}
-                    InputProps={{
-                        readOnly: !learningDayEditable,
-                        classes: { input: classes.title }
-                    }}
-                    size="medium"
-                />
-                <IconButton aria-label="close" className={classes.closeButton} onClick={(e) => handleLearningDayClose(e)}>
-                    <CloseIcon />
-                </IconButton>
-            </DialogTitle>
-            <DialogContent>
-                <Grid container spacing={1} direction="row">
-                    <Grid item xs={9} id="contents">
-                        <Grid container spacing={10} direction="column" justify="space-around">
-                            <Grid item xs={12}>
-                                <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                                    <KeyboardDatePicker
-                                        fullWidth
-                                        disableToolbar
-                                        variant="inline"
-                                        format="yyyy-MM-dd"
-                                        margin="normal"
-                                        label="Date"
-                                        readOnly={learningDayEditable}
-                                        value={date}
-                                        onChange={(date) => setDate(date)}
-                                        KeyboardButtonProps={{
-                                            'aria-label': 'change date',
-                                        }}
-                                    />
-                                </MuiPickersUtilsProvider>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <FormControl className={classes.formControl} fullWidth>
-                                    <InputLabel id="demo-mutiple-chip-label">Topics</InputLabel>
-                                    <Select
-                                        labelId="demo-mutiple-chip-label"
-                                        id="demo-mutiple-chip"
-                                        multiple
-                                        value={topics}
-                                        onChange={(e) => handleTopicsChange(e)}
-                                        open={topicsOpen}
-                                        onOpen={(e) => learningDayEditable ? setTopicsOpen(true) : setTopicsOpen(false)}
-                                        onClose={(e) => setTopicsOpen(false)}
-                                        input={<Input id="select-multiple-chip" />}
-                                        renderValue={(selected) => (
-                                            <div className={classes.chips}>
-                                                {selected.map((topic) => (
-                                                    <Tooltip title={topic.description} arrow>
-                                                        <Chip color="primary" key={topic.id} label={topic.title} className={classes.topicChip} />
-                                                    </Tooltip>
-                                                ))}
-                                            </div>
-                                        )}
-                                    >
-                                        {allTopics.map((topic) => (
-                                            !topic.parentId ?
-                                                <MenuItem key={topic.id} value={topic}>
-                                                    {topic.title}
-                                                </MenuItem> : ''
-                                        ))}
-                                    </Select>
-                                </FormControl>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <FormControl className={classes.formControl} fullWidth>
-                                    <InputLabel id="demo-mutiple-chip-label">Subtopics</InputLabel>
-                                    <Select
-                                        labelId="demo-mutiple-chip-label"
-                                        id="demo-mutiple-chip"
-                                        multiple
-                                        value={subtopics}
-                                        onChange={(e) => handleSubtopicsChange(e)}
-                                        open={subtopicsOpen}
-                                        onOpen={(e) => learningDayEditable ? setSubtopicsOpen(true) : setSubtopicsOpen(false)}
-                                        onClose={(e) => setSubtopicsOpen(false)}
-                                        input={<Input id="select-multiple-chip" />}
-                                        renderValue={(selected) => (
-                                            <div className={classes.chips}>
-                                                {selected.map((topic) => (
-                                                    <Tooltip title={topic.description} arrow>
-                                                        <Chip color="primary" key={topic.id} label={topic.title} className={classes.topicChip} />
-                                                    </Tooltip>
-                                                ))}
-                                            </div>
-                                        )}
-                                    >
-                                        {topics.map((masterTopic) => (
-                                            allTopics.map((topic) => (
-                                                topic.parentId === masterTopic.id ?
+        <div>
+            <Dialog fullWidth="lg" maxWidth="lg" onClose={(e) => handleLearningDayClose(e)} open={(e) => handleLearningDayClose(e)} classes={{ paper: classes.LearningDayModal }}>
+                <DialogTitle className={classes.dialogTitle}>
+                    <TextField
+                        className={classes.title}
+                        required
+                        placeholder="Title"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                        InputProps={{
+                            readOnly: !learningDayEditable,
+                            classes: { input: classes.title }
+                        }}
+                        size="medium"
+                    />
+                    <IconButton aria-label="close" className={classes.closeButton} onClick={(e) => handleLearningDayClose(e)}>
+                        <CloseIcon />
+                    </IconButton>
+                </DialogTitle>
+                <DialogContent>
+                    <Grid container spacing={1} direction="row">
+                        <Grid item xs={9} id="contents">
+                            <Grid container spacing={10} direction="column" justify="space-around">
+                                <Grid item xs={12}>
+                                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                        <KeyboardDatePicker
+                                            fullWidth
+                                            disableToolbar
+                                            variant="inline"
+                                            format="yyyy-MM-dd"
+                                            margin="normal"
+                                            label="Date"
+                                            readOnly={learningDayEditable}
+                                            value={date}
+                                            onChange={(date) => setDate(date)}
+                                            KeyboardButtonProps={{
+                                                'aria-label': 'change date',
+                                            }}
+                                        />
+                                    </MuiPickersUtilsProvider>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <FormControl className={classes.formControl} fullWidth>
+                                        <InputLabel id="demo-mutiple-chip-label">Topics</InputLabel>
+                                        <Select
+                                            labelId="demo-mutiple-chip-label"
+                                            id="demo-mutiple-chip"
+                                            multiple
+                                            value={topics}
+                                            onChange={(e) => handleTopicsChange(e)}
+                                            open={topicsOpen}
+                                            onOpen={(e) => learningDayEditable ? setTopicsOpen(true) : setTopicsOpen(false)}
+                                            onClose={(e) => setTopicsOpen(false)}
+                                            input={<Input id="select-multiple-chip" />}
+                                            renderValue={(selected) => (
+                                                <div className={classes.chips}>
+                                                    {selected.map((topic) => (
+                                                        <Tooltip title={topic.description} arrow>
+                                                            <Chip color="primary" key={topic.id} label={topic.title} className={classes.topicChip} />
+                                                        </Tooltip>
+                                                    ))}
+                                                </div>
+                                            )}
+                                        >
+                                            {allTopics.map((topic) => (
+                                                !topic.parentId ?
                                                     <MenuItem key={topic.id} value={topic}>
-                                                        <Chip color="primary" label={masterTopic.title} className={classes.topicChip} /> {topic.title}
+                                                        {topic.title}
                                                     </MenuItem> : ''
-                                            ))
-                                        ))}
-                                    </Select>
-                                </FormControl>
+                                            ))}
+                                        </Select>
+                                    </FormControl>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <FormControl className={classes.formControl} fullWidth>
+                                        <InputLabel id="demo-mutiple-chip-label">Subtopics</InputLabel>
+                                        <Select
+                                            labelId="demo-mutiple-chip-label"
+                                            id="demo-mutiple-chip"
+                                            multiple
+                                            value={subtopics}
+                                            onChange={(e) => handleSubtopicsChange(e)}
+                                            open={subtopicsOpen}
+                                            onOpen={(e) => learningDayEditable ? setSubtopicsOpen(true) : setSubtopicsOpen(false)}
+                                            onClose={(e) => setSubtopicsOpen(false)}
+                                            input={<Input id="select-multiple-chip" />}
+                                            renderValue={(selected) => (
+                                                <div className={classes.chips}>
+                                                    {selected.map((topic) => (
+                                                        <Tooltip title={topic.description} arrow>
+                                                            <Chip color="primary" key={topic.id} label={topic.title} className={classes.topicChip} />
+                                                        </Tooltip>
+                                                    ))}
+                                                </div>
+                                            )}
+                                        >
+                                            {topics.map((masterTopic) => (
+                                                allTopics.map((topic) => (
+                                                    topic.parentId === masterTopic.id ?
+                                                        <MenuItem key={topic.id} value={topic}>
+                                                            <Chip color="primary" label={masterTopic.title} className={classes.topicChip} /> {topic.title}
+                                                        </MenuItem> : ''
+                                                ))
+                                            ))}
+                                        </Select>
+                                    </FormControl>
+                                </Grid>
                             </Grid>
                         </Grid>
-                    </Grid>
-                    <Grid item xs={3} id="comments">
+                        <Grid item xs={3} id="comments">
 
+                        </Grid>
                     </Grid>
-                </Grid>
-            </DialogContent>
-            <DialogActions>
-                {!learningDayNew && !learningDayEditable ?
-                    <Button autoFocus variant="contained" color="primary" onClick={(e) => handleDeleteLearningDay(e)}>
-                        Delete
+                </DialogContent>
+                <DialogActions>
+                    {!learningDayNew && !learningDayEditable ?
+                        <Button autoFocus variant="contained" color="primary" onClick={(e) => setDeleteDialog(true)}>
+                            Delete
                     </Button> : ''
-                }
-                {learningDayNew ?
-                    <Button autoFocus variant="contained" color="primary" onClick={(e) => handleCreateLearningDay(e)}>
-                        Create Learning Day
+                    }
+                    {learningDayNew ?
+                        <Button autoFocus variant="contained" color="primary" onClick={(e) => handleCreateLearningDay(e)}>
+                            Create Learning Day
                     </Button> :
-                    learningDayEditable ?
-                        <Button autoFocus variant="contained" color="primary" onClick={(e) => handleSaveLearningDay(e)}>
-                            Save
+                        learningDayEditable ?
+                            <Button autoFocus variant="contained" color="primary" onClick={(e) => handleSaveLearningDay(e)}>
+                                Save
                         </Button> :
-                        <Button autoFocus variant="contained" color="primary" onClick={(e) => setLearningDayEditable(true)}>
-                            Edit
+                            <Button autoFocus variant="contained" color="primary" onClick={(e) => setLearningDayEditable(true)}>
+                                Edit
                         </Button>
-                }
-            </DialogActions>
-        </Dialog>
+                    }
+                </DialogActions>
+            </Dialog>
+            <Dialog
+                open={deleteDialog}
+                onClose={(e) => setDeleteDialog(false)}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">{"Are you sure you want to delete this Learning Day?"}</DialogTitle>
+                <DialogActions>
+                    <Button onClick={(e) => handleDeleteLearningDay(e)} color="primary">
+                        Yes
+                </Button>
+                    <Button onClick={(e) => setDeleteDialog(false)} color="primary" autoFocus>
+                        No
+                </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
     );
 }
 
